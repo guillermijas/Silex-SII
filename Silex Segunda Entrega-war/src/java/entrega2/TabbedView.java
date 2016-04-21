6255250db75f60a56db3b9390cd6455eaa4296ed
alumno <alumno@pc0415.docencia.lcc.uma.es>
2016-04-21 10:23:34 +0200

    borrado lista clientes

--- Silex Segunda Entrega-war/src/java/entrega2/TabbedView.java
+++ Silex Segunda Entrega-war/src/java/entrega2/TabbedView.java
@@ -1,9 +1,5 @@
 package entrega2;
 
-import entrega1.Cliente;
-import java.util.ArrayList;
-import java.util.List;
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
@@ -14,19 +10,7 @@ import org.primefaces.event.TabCloseEvent;
 @ManagedBean
 public class TabbedView {
      
-    private List<Cliente> cars;
- 
-    @PostConstruct
-    public void init() {
-        cars = new ArrayList<Cliente>();
-        cars.add(new Cliente());
-        cars.add(new Cliente());
-    }
-     
-    public List<Cliente> getCars() {
-        return cars;
-    }
-     
+   
     public void onTabChange(TabChangeEvent event) {
         FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
         FacesContext.getCurrentInstance().addMessage(null, msg);
